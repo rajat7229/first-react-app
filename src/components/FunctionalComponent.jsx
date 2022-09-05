e9ef6bf541192e7c98022c7d094a4b9ca798f414
Rajat Kumar Singh <rajat7229@gmail.com>
2022-09-05 18:58:36 +0530

    just to learn

---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FunctionalComponent () {
+    return(
+        <div>
+            <p>This is Functional Component</p>
+        </div>
+    )
+}
+
+export default FunctionalComponent;
