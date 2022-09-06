7526f5926900ec311cf3804ceaf850741932e22d
Rajat Kumar Singh <rajat7229@gmail.com>
2022-09-06 20:01:39 +0530

    States and Props in React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import ClassComponent from './components/ClassComponent';
 import FunctionalComponent from './components/FunctionalComponent';
 
 function App() {
+  const [name, setName] = useState('Rajat');
+
   return (
     <div className="App">
       <h1>Class Component</h1>
       <ClassComponent></ClassComponent>
       <br />
       <h1>Functional Component</h1>
-      <FunctionalComponent></FunctionalComponent>
+      <FunctionalComponent name={name} age={'20'} company={'Devtown'} setName={setName}></FunctionalComponent>
     </div>
   );
 }
--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function FunctionalComponent () {
+const FunctionalComponent = (props) => {
+
+    const [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("");
+
     return(
         <div>
             <p>This is Functional Component</p>
+            <button onClick={() => setCount(count + 1)}>Click me to Add 1</button>
+            <button onClick={() => setCount(count - 1)}>Click me to Subtract 1</button>
+            <h1>{count}</h1>
+            <p>Above line is using state(usestate)</p>
+            <h1>My name is {props.name}, I am {props.age}, I work for {props.company}</h1>
+            <p>Above line is using props</p>
+            <input onChange={(e) => setChangeName(e.target.value)} />
+            <button onClick={() => props.setName(changeName)}>Change name</button>
+            <p>Above line is using both props and states</p>
         </div>
     )
 }
